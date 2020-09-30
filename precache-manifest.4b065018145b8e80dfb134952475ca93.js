d15b94e2e87ee9cc40496edf11a65227f2fc301a
Vladislav Grents <vladair_o@hotmail.com>
2020-09-30 15:52:10 +0500

    Updates

---
+++ precache-manifest.4b065018145b8e80dfb134952475ca93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe41f7c8f9167977ba7287396f72b3de",
+    "url": "/social-network-app/index.html"
+  },
+  {
+    "revision": "969e169ea43e2cba9757",
+    "url": "/social-network-app/static/css/3.4b1606a2.chunk.css"
+  },
+  {
+    "revision": "c705121dacb8ac594487",
+    "url": "/social-network-app/static/css/main.7cafd51b.chunk.css"
+  },
+  {
+    "revision": "6bab837f526b3d2af4d5",
+    "url": "/social-network-app/static/js/2.9993b9dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-app/static/js/2.9993b9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969e169ea43e2cba9757",
+    "url": "/social-network-app/static/js/3.f3484a99.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/social-network-app/static/js/3.f3484a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c705121dacb8ac594487",
+    "url": "/social-network-app/static/js/main.c3154462.chunk.js"
+  },
+  {
+    "revision": "7a8c9b69d8175da95fd2",
+    "url": "/social-network-app/static/js/runtime-main.f8cec263.js"
+  },
+  {
+    "revision": "de3c9476ee3d105023c3e5384ee18ce7",
+    "url": "/social-network-app/static/media/preloader.de3c9476.svg"
+  },
+  {
+    "revision": "2afd1c89ddfde45be1416b85eec18ce2",
+    "url": "/social-network-app/static/media/user.2afd1c89.png"
+  }
+]);
